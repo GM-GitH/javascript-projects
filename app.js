5fcf5a35c28ca7b7301f9609cf5bc7888f66cd5c
GM-GitH <72597058+GM-GitH@users.noreply.github.com>
2022-07-28 12:52:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const { checkInventory } = require("./library.js");
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
 
-const order = [
-  ["sunglasses", 1],
-  ["bags", 2],
-];
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
 
-const handleSuccess = (resolved) => {
-  console.log(resolved);
-};
-const handleFailure = (rejected) => {
-  console.log(rejected);
-};
-checkInventory(order).then(handleSuccess, handleFailure);
+checkInventory(order)
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+ return processPayment(resolvedValueArray)
+})
+.then((resolvedValueArray) => {
+  // Write the correct return statement here:
+  return shipOrder(resolvedValueArray)
+})
+.then((successMessage) => {
+  console.log(successMessage);
+})
+.catch((errorMessage) => {
+  console.log(errorMessage);
+});
